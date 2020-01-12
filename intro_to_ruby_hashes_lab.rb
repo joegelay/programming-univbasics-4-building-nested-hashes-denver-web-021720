4ca08a81ade12b9070cc0c939596da95b5e63632
Joe Gelay <joegelay@gmail.com>
2020-01-12 20:52:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   
    {railroads: {
     :pieces => 4,
-    :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :key4 => "1"},
+    :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => "1"},
     :names => {:key => "1", :key2 => "1", :key3 => "1", :key4 => "1"}
   }
      
